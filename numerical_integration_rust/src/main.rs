39f5bb1a1986f39d88b6662fa5fe54937007da41
Shuyang Sun <shuyang@havenlife.com>
2020-08-05 11:30:11 -0400

    Add Rust benchmarks.

--- numerical_integration_rust/src/main.rs
+++ numerical_integration_rust/src/main.rs
@@ -1,5 +1,4 @@
 use integration_rust::IntegrationFloat;
-use num_traits::NumCast;
 
 fn func_1<T>(x: T) -> T
 where
@@ -8,22 +7,8 @@ where
     x
 }
 
-fn func_2<T>(x: T) -> T
-where
-    T: IntegrationFloat,
-{
-    x * x
-}
-
-fn func_3<T>(x: T) -> T
-where
-    T: IntegrationFloat,
-{
-    x * x * x * NumCast::from(5.0f32).unwrap() - NumCast::from(8.0f32).unwrap()
-}
-
 fn main() {
     let mut romberg_calc = integration_rust::Romberg::from(func_1, 0.0, 2.0);
-    let res = romberg_calc.calculate(10, 10);
+    let res = romberg_calc.calculate(10, 100);
     println!("{}", res);
 }
