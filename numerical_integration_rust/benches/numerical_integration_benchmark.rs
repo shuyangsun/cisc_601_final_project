90096b9e9f0dfad9fccca1df929ca7ce0e7b4228
Shuyang Sun <sunbuffett@gmail.com>
2020-08-05 20:17:05 -0400

    Finish adding benchmark results.

--- numerical_integration_rust/benches/numerical_integration_benchmark.rs
+++ numerical_integration_rust/benches/numerical_integration_benchmark.rs
@@ -2,10 +2,7 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use integration_rust::{simpson_1_3, simpson_3_8, trapezoidal, IntegrationFloat};
 use num_traits::NumCast;
 
-const LEFT_32: f32 = 0.0f32;
 const LEFT_64: f64 = 0.0f64;
-
-const RIGHT_32: f32 = 5.0f32;
 const RIGHT_64: f64 = 5.0f64;
 
 const NUM_SECTIONS: [u16; 13] = [1, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
@@ -15,29 +12,6 @@ where
     T: IntegrationFloat,
 {
     x * x * x * NumCast::from(5.0f32).unwrap() - NumCast::from(8.0f32).unwrap()
-}
-
-fn f32_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("f32");
-
-    for num_section in NUM_SECTIONS.iter() {
-        group.bench_with_input(
-            BenchmarkId::new("Trapezoidal", num_section),
-            num_section,
-            |b, _| b.iter(|| trapezoidal(func, *num_section, LEFT_32, RIGHT_32)),
-        );
-        group.bench_with_input(
-            BenchmarkId::new("Simpson's 1/3", num_section),
-            num_section,
-            |b, _| b.iter(|| simpson_1_3(func, *num_section, LEFT_32, RIGHT_32)),
-        );
-        group.bench_with_input(
-            BenchmarkId::new("Simpson's 3/8", num_section),
-            num_section,
-            |b, _| b.iter(|| simpson_3_8(func, *num_section, LEFT_32, RIGHT_32)),
-        );
-    }
-    group.finish();
 }
 
 fn f64_benchmark(c: &mut Criterion) {
@@ -63,5 +37,5 @@ fn f64_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, f32_benchmark, f64_benchmark);
+criterion_group!(benches, f64_benchmark);
 criterion_main!(benches);
